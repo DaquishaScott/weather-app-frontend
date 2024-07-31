2cba9de55262034871c038984ea53dce0a064831
Daquisha <dscott.prsvr@gmail.com>
2024-07-31 14:58:49 -0500

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header/Header'
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
